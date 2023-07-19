69132777d79a16bff95f5bcb5140ce5ad85cf250
Melanie Wilson <melaniemwilson1@gmail.com>
2023-07-19 13:24:50 -0400

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    new_num_list = []
+    i=0
+    while i < len(num_list):
+        if (num_list[i]%2 == 0):
+            new_num_list.append(num_list[i])
+        i += 1
+    return new_num_list
 
 def make_exclamation(sentence_list):
-    pass
+    i=0
+    while i < len(sentence_list):
+        sentence_list[i] += "!"
+        i += 1
+    return sentence_list
